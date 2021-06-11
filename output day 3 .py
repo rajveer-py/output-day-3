4b727920aca412dd6db3b26a13e76aa816844e8f
rajveer-py <85725277+rajveer-py@users.noreply.github.com>
2021-06-11 21:36:22 +0530

    Add files via upload

---
+++ output day 3 .py
@@ -0,0 +1,15 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/tcsc/AppData/Local/Programs/Python/Python39/calculator.py day3.py
+Enter your first number: 5
+Enter your second number: 5
+5 + 5 = 
+10
+5 - 5 = 
+0
+5 * 5 = 
+25
+5 / 5 = 
+1.0
+>>> 
